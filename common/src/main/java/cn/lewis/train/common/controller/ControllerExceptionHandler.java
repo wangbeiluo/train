013683f9dab3ff29e4e94c49025e464f3bce3fc0
Mathew_Lewis <1664181211@qq.com>
2023-06-12 22:54:30 +0800

    feat: 为项目增加统一的异常处理

---
+++ common/src/main/java/cn/lewis/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package cn.lewis.train.common.controller;
+
+import cn.lewis.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author lewis
+ * @description: 统一异常处理
+ * @date: 2023/6/12
+ * @Copyright:
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler
+{
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getMessage());
+        return commonResp;
+    }
+}
