8879b0433b003845ea3d21e854b25c98f9ce306b
Mathew_Lewis <1664181211@qq.com>
2023-06-12 15:07:32 +0800

    feat: 初始化项目

---
+++ src/main/java/cn/lewis/train/TrainApplication.java
@@ -0,0 +1,15 @@
+package cn.lewis.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lewis/train/TrainApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.lewis.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
